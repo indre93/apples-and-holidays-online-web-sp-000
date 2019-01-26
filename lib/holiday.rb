a8fcf5d9aaae4570e0840ae6eb8cf3237a6a62bf
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-26 02:05:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   array = []
   holiday_hash.each do |key, value|
-    value.each {|key2, value2| array << key2 if value2 == "BBQ"}
+    value.each {|key2, value2| array << key2 }
 
   end
   array
